aa622e31fbc23bb40eb5c8c7855c30c5b91df13d
SeifSultan03 <99747202+SeifSultan03@users.noreply.github.com>
2024-08-20 12:41:03 -0500

    Subpages
    - Added subpages template
    - edited animations

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = [...document.querySelectorAll('.hidden'), ... document.querySelectorAll('.hiddenRight')]
 hiddenElements.forEach(element => {observer.observe(element)});
+
+
+
+// make biuttons same height
+/*window.onload = function() {
+    const imageItems = document.querySelectorAll('.image-item');
+
+    imageItems.forEach(function(item) {
+        const img = item.querySelector('.dynamicImage');
+        const buttonsDiv = item.querySelector('.buttons');
+
+        // Get the height of the image
+        const imgHeight = img.clientHeight;
+
+        // Set the margin-top of the buttons div to the height of the image
+        buttonsDiv.style.marginTop = (213 - imgHeight + 15) + 'px';
+    });
+};*/
